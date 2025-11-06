f506823d9d659799fe0cda7d7725adf9e47100f8
ridham sharma <ridham.sharmarce@gmail.com>
2025-11-06 10:56:54 +0530

    add-backend-common-folder

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("signup",(req,res)=>{
+    
+})
+
+app.post("signin",(req,res)=>{
+
+})
+app.post("room",(req,res)=>{
+    
+})
